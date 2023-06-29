f6d0d30b291c4e8d0f6433aeef6860d810cc06d1
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-29 22:05:28 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,81 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { createContext, useState } from 'react';
 
+const AppContext = createContext();
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [itemList, setItemList] = useState([]);
+
+  const handleLogin = () => {
+    setCurrentUser('rohan');
+    setIsAuthenticated(true);
+  };
+
+  const handleSignout = () => {
+    setCurrentUser('');
+    setIsAuthenticated(false);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== '') {
+      const newItem = {
+        id: `item-${inputValue}`,
+        name: inputValue,
+      };
+      setItemList((prevItems) => [...prevItems, newItem]);
+      setInputValue('');
+    }
+  };
+
+  const handleRemoveItem = (itemId) => {
+    setItemList((prevItems) => prevItems.filter((item) => item.id !== itemId));
+  };
+
+  const handleClearList = () => {
+    setItemList([]);
+  };
+
+  return (
+    <AppContext.Provider value={{ currentUser, isAuthenticated }}>
+      <div>
+        <button id="login-btn" onClick={handleLogin}>
+          Login
+        </button>
+        <button id="signout" onClick={handleSignout}>
+          Signout
+        </button>
+        <div id="current-user">
+          Current user: {currentUser}, isAuthenticated: {isAuthenticated ? 'Yes' : 'No'}
+        </div>
+        <input
+          id="shopping-input"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddItem}>Add</button>
+        <ul>
+          {itemList.map((item) => (
+            <li key={item.id}>
+              {item.name}
+              <button id={`remove-${item.id}`} onClick={() => handleRemoveItem(item.id)}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button id="clear-list" onClick={handleClearList}>
+          Clear
+        </button>
+      </div>
+    </AppContext.Provider>
+  );
+};
+
+export default App;
